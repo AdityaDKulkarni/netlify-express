9c5fde43dbe3b687204282374d3081b8281941ce
AdityaDKulkarni <42543219+AdityaDKulkarni@users.noreply.github.com>
2020-04-06 11:37:49 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<textarea rows="100" columns="100">Hello from Express.js!</textarea>');
-  res.write('<h1>Hello</h1>')
+  res.write('<h1>Hello</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
